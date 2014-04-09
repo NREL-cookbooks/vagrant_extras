36f43d66fa52edd8e7ba297dfd55eecda88aa685
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-09 16:32:48 -0600

    Adding name to metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "vagrant_extras"
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
 description      "Extra fixes and shims for running your recipes in a Vagrant VM."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 
 supports "ubuntu"
 supports "debian"
